50c2a6c56f871bb49c810720aa90601f431cc61b
Shinya Tsuda <shinya@dacci.org>
2023-06-14 23:23:53 +0900

    chore(deps): upgrade dependencies

--- .projenrc.ts
+++ .projenrc.ts
@@ -41,7 +41,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     `,
   },
 
-  cdkVersion: '2.74.0',
+  cdkVersion: '2.83.1',
   deps: [
     '@aws-sdk/client-ec2',
     '@aws-sdk/client-route-53',
