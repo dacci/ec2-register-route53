e1876055484415e3cbca3ad7babb34610d1320c1
Shinya Tsuda <r.shinya.tsuda@gmail.com>
2026-03-12 11:07:12 +0900

    ci: define workflows using projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,6 @@
 import { awscdk } from 'projen';
+import { JobPermission } from 'projen/lib/github/workflows-model';
+
 const project = new awscdk.AwsCdkTypeScriptApp({
   name: 'ec2-register-route53',
   copyrightOwner: 'Shinya Tsuda',
@@ -51,4 +53,76 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     'aws-sdk-client-mock-jest',
   ],
 });
+
+const deploy = project.github?.addWorkflow('deploy');
+deploy?.on({
+  push: {
+    branches: ['main'],
+  },
+});
+deploy?.addJob('deploy', {
+  permissions: {
+    contents: JobPermission.WRITE,
+    idToken: JobPermission.WRITE,
+  },
+  runsOn: ['ubuntu-latest'],
+  steps: [
+    {
+      name: 'Checkout',
+      uses: 'actions/checkout@v6'
+    },
+    {
+      name: 'Setup Node.js',
+      uses: 'actions/setup-node@v6',
+      with: {
+        'node-version': 24,
+        cache: 'yarn'
+      }
+    },
+    {
+      name: 'Install dependencies',
+      run: 'yarn install'
+    },
+    {
+      name: 'Run tests',
+      run: 'yarn test'
+    },
+    {
+      name: 'Configure AWS credentials',
+      uses: 'aws-actions/configure-aws-credentials@v6',
+      with: {
+        'role-to-assume': '${{ secrets.ACTIONS_ROLE_ARN }}',
+        'aws-region': '${{ vars.AWS_REGION }}'
+      }
+    },
+    {
+      name: 'Deploy',
+      run: 'yarn cdk deploy --ci --require-approval never'
+    },
+  ],
+});
+
+const mergeDependabotPr = project.github?.addWorkflow('merge-dependabot-pr');
+mergeDependabotPr?.on({
+  pullRequest: {},
+});
+mergeDependabotPr?.addJob('dependabot', {
+  permissions: {
+    pullRequests: JobPermission.WRITE,
+    contents: JobPermission.WRITE,
+  },
+  if: "${{ github.actor == 'dependabot[bot]' }}",
+  runsOn: ['ubuntu-latest'],
+  steps: [
+    {
+      name: 'Merge Dependabot PR',
+      env: {
+        PR_URL: '${{ github.event.pull_request.html_url }}',
+        GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
+      },
+      run: 'gh pr merge --auto --merge "$PR_URL"',
+    },
+  ],
+});
+
 project.synth();
